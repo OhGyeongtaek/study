1ff00710f845a531807235e29df01a636944c2d7
Oh Gyeong Taek <rudxor974@gmail.com>
2021-05-24 17:05:32 +0900

    리드미 수정

--- Typescript/code/CoffeeMachine2.ts
+++ Typescript/code/CoffeeMachine2.ts
@@ -7,15 +7,11 @@
   class CoffeeMachine {
     //static을 사용한 이유는 CoffeeMachine가 선언될때마다
     //메모리에 BEANS_GRAMM_PER_SHOT가 추가됨
-    static BEANS_GRAMM_PER_SHOT: number = 7;
+    private static BEANS_GRAMM_PER_SHOT: number = 7;
 
-    coffeeBeans = 0;
+    private coffeeBeans = 0;
 
-    constructor(coffeeBeans: number) {
-      this.coffeeBeans = coffeeBeans * CoffeeMachine.BEANS_GRAMM_PER_SHOT;
-    }
-
-    public makeCoffee(shots: number): CoffeeCup {
+    makeCoffee(shots: number): CoffeeCup {
       if (this.coffeeBeans < shots * CoffeeMachine.BEANS_GRAMM_PER_SHOT) {
         throw new Error("커피가 없어요!");
       }
@@ -27,9 +23,17 @@
         hasMilk: false,
       };
     }
+
+    fillCoffeeBeans(beans: number) {
+      if (beans < 0) {
+        throw new Error("커피는 1보다 이상 넣어주세요");
+      }
+
+      this.coffeeBeans += beans;
+    }
   }
 
-  const machine = new CoffeeMachine(10);
+  const machine = new CoffeeMachine();
 
   console.log(machine.makeCoffee(5));
 }
